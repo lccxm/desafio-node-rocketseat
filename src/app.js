265fb20055f862f4b6f9484750f851a06e4af9ae
lccxm <luccamolon@gmail.com>
2020-11-05 13:05:57 -0300

    pronto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,61 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repositorie = {id: uuid(), title, url, techs, likes: 0};
+  repositories.push(repositorie);
+  return response.status(201).json(repositorie);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+  const repositorieIndex = repositories.findIndex(respositorie => respositorie.id === id);
+  
+
+  if(repositorieIndex < 0){
+    return response.sendStatus(400).json({error: "id do not exist"});
+  }
+  const previousLikes = repositories[repositorieIndex].likes;
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: previousLikes 
+  }
+  repositories[repositorieIndex] = repositorie;
+  return response.json(repositorie);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositorieIndex = repositories.findIndex(respositorie => respositorie.id === id);
+
+  if(repositorieIndex < 0){
+    return response.sendStatus(400).json({error: "id do not exist"});
+  }
+
+  repositories.splice(repositorieIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositorieIndex = repositories.findIndex(respositorie => respositorie.id === id);
+
+  if(repositorieIndex < 0){
+    return response.sendStatus(400).json({error: "id do not exist"});
+  }
+
+  repositories[repositorieIndex].likes += 1;
+  return response.json(repositories[repositorieIndex]);
 });
 
 module.exports = app;
